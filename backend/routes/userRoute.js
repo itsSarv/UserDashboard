7e8caa1c5be0cb14f02dc4b40e0621d296841254
saurav tiwari <sauravtiwari145@gmail.com>
2024-04-29 22:45:33 +0545

    updated backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,15 +2,17 @@ const router = require('express').Router()
 const mongoose = require('mongoose')
 const User = require("../modules/usermodel");
 
+
+//create
 router.post("/", async(req,res)=>{
     const {name,email,age} = req.body;
     try {
-      const userData = await User.create({
+      const userAdded = await User.create({
       name,
       email,
       age
     });
-    res.status(201).json(userData)
+    res.status(201).json(userAdded)
     }
     catch (error) {
       console.log(error)
@@ -18,7 +20,7 @@ router.post("/", async(req,res)=>{
     }
   })
   
-  
+  //get
   router.get('/', async(req, res) => {
     try {
       const showAll = await User.find();
@@ -30,4 +32,47 @@ router.post("/", async(req,res)=>{
     }
   })
 
+  //get single user
+  router.get('/:id', async(req, res) => {
+    const {id} = req.params;
+    try {
+      const singleuser = await User.findById({_id : id});
+      res.status(200).json(singleuser);
+    }
+    catch (error) {
+      console.log(error)
+      res.status(500).json({error:error.message})
+    }
+  })
+
+    //delete 
+    router.delete('/:id', async(req, res) => {
+      const {id} = req.params;
+      try {
+        const singleuser = await User.findByIdAndDelete({_id : id});
+        res.status(200).json(singleuser);
+      }
+      catch (error) {
+        console.log(error)
+        res.status(500).json({error:error.message})
+      }
+    })
+  
+
+        //put/patch 
+        router.patch('/:id', async(req, res) => {
+          const {id} = req.params;
+          const{name, email, age} = req.body;
+          try {
+            const updateUser = await User.findByIdAndUpdate(id, req.body, {
+              new: true,
+            });
+            res.status(200).json(updateUser);
+          }
+          catch (error) {
+            console.log(error)
+            res.status(500).json({error:error.message})
+          }
+        })
+      
 module.exports = router;
